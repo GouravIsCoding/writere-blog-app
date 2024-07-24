f9712021995401fc67eb8e7194b74c5a51fdd2bd
GouravisCoding <pythonicgourav@gmail.com>
2024-07-24 20:32:58 +0530

    fixed a bug where created blogs would not appear immediately on bloglist pages due to caching

--- src/app/actions/createBlog.ts
+++ src/app/actions/createBlog.ts
@@ -5,6 +5,7 @@ import { asyncHandler } from "@/utils/asyncHandler";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/option";
 import { createBlogDb } from "@/db/blog/create";
+import { revalidatePath } from "next/cache";
 
 export async function createBlogAction(title: string, contents: contentType[]) {
   return asyncHandler(async () => {
@@ -15,6 +16,9 @@ export async function createBlogAction(title: string, contents: contentType[]) {
     if (!session?.user.id) return { error: "User not logged in!" };
 
     const createdBlog = await createBlogDb(title, session?.user.id, contents);
+
+    revalidatePath("/blogs");
+    revalidatePath("/dashboard/myblog");
 
     return {
       data: { blogId: createdBlog.id },
